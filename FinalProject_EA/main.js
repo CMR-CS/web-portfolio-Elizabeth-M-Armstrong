3d62f7b29ba7b5ccd0f766a590f0538f9b3abafd
Elizabeth-M-Armstrong <46968175+Elizabeth-M-Armstrong@users.noreply.github.com>
2019-05-21 09:49:58 -0600

    5/21/19 - Final

---
+++ FinalProject_EA/main.js
@@ -0,0 +1,14 @@
+/*
+ * Auto-generated content from the Brackets New Project extension.
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global $, window, document */
+
+// Simple jQuery event handler
+$(document).ready(function () {
+    "use strict";
+    $("#clickMe").click(function (e) {
+        alert("Welcome to my page, here you will find information on galaxies, planets, and things that happen in space.");
+    });
+});
